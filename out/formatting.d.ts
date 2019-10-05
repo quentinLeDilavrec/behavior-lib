9da4102d18bc91802d80caf7c0f532d305cfb8b9
quentin <ledilavrecquentin@gmail.com>
2019-10-06 01:10:34 +0200

    compile

--- out/formatting.d.ts
+++ out/formatting.d.ts
@@ -1,5 +1,6 @@
 /// <reference types="node" />
+import stream = require('stream');
 import { Writable } from "stream";
 export declare function ltreeFormat(s: string): string;
 export declare function reformatFileOld(inPath: string[] | string, output: string | Writable, getSession: number | ((i: number) => number), pattern?: RegExp): void;
-export declare function reformatFile(input: [string, number][]): Writable;
+export declare function reformatFile(input: [string, number][]): stream.Readable;
